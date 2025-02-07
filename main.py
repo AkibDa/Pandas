6f43331e953335efa9179846b06a4e8b50ed265f
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-02-07 11:50:28 +0530

    exporting

--- main.py
+++ main.py
@@ -29,4 +29,33 @@ ex2 = pd.read_csv("data/data.csv")
 print(ex2.head())
 print(ex2.dropna())
 print(ex2.fillna(0))
-print(ex2.rename(columns={"SepalLengthCm": "SL"}))
+print(ex.rename(columns={"SepalLengthCm": "SL"}))
+print(ex.describe())
+print(ex.info())
+# ex["SL"] = ex["SL"].astype(int)
+# print(ex.info())
+
+ex["zeros"] = [0 for i in range(len(ex))] 
+print(ex)
+
+def fx(a):
+  return a+1
+
+ex["zeros + 1"] = ex["zeros"].apply(fx)
+print(ex["zeros + 1"])
+
+ex.to_csv("data/export.csv", index=False)
+
+ex1 = pd.DataFrame({
+  "name": ["Akib","Rohit","Debom","Barnob","Shuvayan","Shubham"],
+  "marks": [100,86,54,68,92,77],
+})
+print(ex1)
+
+ex3 = pd.DataFrame({
+  "name": ["Shail","Nikita","Saloni","Ananya","Sneha","Shreya"],
+  "marks": [81,78,66,79,72,67],
+})
+print(ex3)
+
+pd.concat(ex1, ex3)
