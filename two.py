52ecca4fbb9a6f91d74c6b68d95fdbba376c59df
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-02-15 21:58:26 +0530

    New course

---
+++ two.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data = {"Name": ["Akib","Mahroof","Rishi"],
+        "Age": ["18","19","20"],
+        "Salary": [45000,25000,30000],}
+
+df = pd.DataFrame(data)
+
+print(df)
+
+data = pd.read_csv("Data/Iris.csv")
+print(data)
