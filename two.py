f766286329a37aa8cafc4f701fa74e3d715c9b37
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-02-16 18:56:30 +0530

    Working with missing data

--- two.py
+++ two.py
@@ -11,14 +11,19 @@ print(df)
 data = pd.read_csv("Data/daily_food_nutrition_dataset.csv")
 print(data)
 
-# Understanding Data
+# Understanding data
 print(data.head(10))
 print(data.tail(10))
 print(data.info())
 print(data.describe())
 print(data.isnull().sum())
 
-#Handling duplicate values
+# Handling duplicate values
 print(data.duplicated().sum())
-print(data.drop_duplicates("")) # give name of the field where u want to delete the duplicate value
+# print(data.drop_duplicates("")) give name of the field where u want to delete the duplicate value
+
+# Working with missing data
+print(data.isnull().sum())
+print(data.dropna())
+# print(data.replace(np.nan,"hi"))
 
